aa36779482e91fd1f5cf4c98feb6ba53192db45d
Anna Garcia <annaegarcia@gmail.com>
2018-01-23 23:21:10 -0500

    refactor page template

---
+++ src/components/PageTemplate.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { nativeDevice } from '../utils';
+import './PageTemplate.css';
+
+const addNativeClass = nativeDevice ? 'native-content' : '';
+
+const PageTemplate = ({
+  title,
+  intro,
+  imgSrc,
+  imgAlt = 'a picture of Anna',
+  copy
+}) => (
+  <section className={`main ${addNativeClass}`}>
+    <div className="text-main">
+      <h1 className={`title ${addNativeClass}`}>{title}</h1>
+      <p className={`intro ${addNativeClass}`}>{intro}</p>
+      {nativeDevice && <hr />}
+      {nativeDevice && (
+        <div>
+          <img
+            src={imgSrc}
+            className={`image ${addNativeClass}`}
+            alt={imgAlt}
+          />
+        </div>
+      )}
+      {nativeDevice && <hr />}
+      <div className={`copy ${addNativeClass}`}>{copy}</div>
+    </div>
+    {!nativeDevice && (
+      <div>
+        <img src={imgSrc} className={`image ${addNativeClass}`} alt={imgAlt} />
+      </div>
+    )}
+  </section>
+);
+
+export default PageTemplate;
