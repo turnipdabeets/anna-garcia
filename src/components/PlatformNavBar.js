112a90f4bccad2317587bf9c51a1ffee39209d62
Anna Garcia <annaegarcia@gmail.com>
2018-01-26 23:01:56 -0500

    refactor navbar and add active class when route it hit

--- src/components/PlatformNavBar.js
+++ src/components/PlatformNavBar.js
@@ -1,58 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { nativeDevice } from '../utils';
 import './PlatformNavBar.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-const NavBar = () => {
-  let deviceClass;
-  if (nativeDevice) {
-    deviceClass = 'nativeRouteGroup';
-  } else {
-    deviceClass = 'routeGroup';
-  }
+const activeColor = '#f40ef0';
+const deviceClass = nativeDevice ? 'nativeRouteGroup' : 'routeGroup';
 
-  return (
-    /*eslint-disable jsx-a11y/no-redundant-roles */
-    <nav role="navigation" className="navigation">
-      <ul className={deviceClass}>
-        {nativeDevice && (
-          <li>
-            <Link to="/">
-              <i className="fa fa-home" aria-hidden="true" />
-              <p>home</p>
-            </Link>
-          </li>
-        )}
-        <li>
-          <Link to="/about">
-            {nativeDevice && <i className="fa fa-info" aria-hidden="true" />}
-            <p>about</p>
-          </Link>
-        </li>
-        <li>
-          <Link to="/music">
-            {nativeDevice && <i className="fa fa-music" aria-hidden="true" />}
-            <p>music</p>
-          </Link>
-        </li>
-        <li>
-          <Link to="/code">
-            {nativeDevice && <i className="fa fa-code" aria-hidden="true" />}
-            <p>code</p>
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact">
-            {nativeDevice && (
-              <i className="fa fa-envelope" aria-hidden="true" />
-            )}
-            <p>contact</p>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const sharedPaths = [
+  {
+    name: 'about',
+    to: '/about',
+    icon: 'fa fa-info'
+  },
+  {
+    name: 'music',
+    to: '/music',
+    icon: 'fa fa-music'
+  },
+  {
+    name: 'code',
+    to: '/code',
+    icon: 'fa fa-code'
+  },
+  {
+    name: 'contact',
+    to: '/contact',
+    icon: 'fa fa-envelope'
+  }
+];
+
+const createLinks = paths =>
+  paths.map(path => (
+    <li key={path.name}>
+      <NavLink exact to={path.to} activeStyle={{ color: activeColor }}>
+        {nativeDevice && <i className={path.icon} aria-hidden="true" />}
+        <p>{path.name}</p>
+      </NavLink>
+    </li>
+  ));
+
+const NavBar = () => (
+  /*eslint-disable jsx-a11y/no-redundant-roles */
+  <nav role="navigation" className="navigation">
+    <ul className={deviceClass}>
+      {nativeDevice &&
+        createLinks([{ name: 'home', to: '/', icon: 'fa fa-home' }])}
+      {createLinks(sharedPaths)}
+    </ul>
+  </nav>
+);
 
 export default NavBar;
